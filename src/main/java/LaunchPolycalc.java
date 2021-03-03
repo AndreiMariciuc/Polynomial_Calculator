0b7850e7241f1f198ed46e776298fc69667a7ea3
Andrei-Alexandru Mariciuc <mariciucandreii@gmail.com>
2021-03-03 21:45:27 +0200

    initial commit

--- src/main/java/LaunchPolycalc.java
+++ src/main/java/LaunchPolycalc.java
@@ -1,5 +1,11 @@
+import controllerpolycalc.ControllerPolycalc;
+import modelpolycalc.ModelPolycalc;
+import viewpolycalc.ViewPolycalc;
+
 public class LaunchPolycalc {
     public static void main(String[] args) {
-
+        ModelPolycalc modelPolycalc = new ModelPolycalc();
+        ViewPolycalc viewPolycalc = new ViewPolycalc();
+        ControllerPolycalc controllerPolycalc = new ControllerPolycalc(viewPolycalc, modelPolycalc);
     }
 }
--- src/main/java/modelpolycalc/ModelPolycalc.java
+++ src/main/java/modelpolycalc/ModelPolycalc.java
@@ -4,7 +4,5 @@ import controllerpolycalc.ControllerPolycalc;
 import viewpolycalc.ViewPolycalc;
 
 public class ModelPolycalc {
-    ModelPolycalc modelPolycalc = new ModelPolycalc();
-    ViewPolycalc viewPolycalc = new ViewPolycalc();
-    ControllerPolycalc controllerPolycalc = new ControllerPolycalc(viewPolycalc, modelPolycalc);
+
 }
